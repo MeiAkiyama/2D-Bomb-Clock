c1dad2f4294bcffb9cd29aaaf452aaee8dc30fd1
MeiAkiyama <akimei514@gmail.com>
2024-06-17 16:59:09 +0900

    線の色を識別するコンポーネント作成

---
+++ Assets/Scripts/lineManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineManager : MonoBehaviour
+{
+    public enum lineColors{
+        Red,Blue,Green,Yellow
+    };
+}
