95a2e6e693a62ff6b08b349e9e21202bec5de437
MeiAkiyama <akimei514@gmail.com>
2024-06-18 18:34:15 +0900

    説明シーンレイアウト作成

--- Assets/Scripts/TimeSet.cs
+++ Assets/Scripts/TimeSet.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using TMPro;
 
 public class TimeSet : MonoBehaviour
 {
     private static int hourNumber;
     private int minuteNumber;
+
+    [SerializeField] GameObject text1;
+    [SerializeField] GameObject text2;
+
+    private TextMeshProUGUI tex1;
+    private TextMeshProUGUI tex2;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +22,13 @@ public class TimeSet : MonoBehaviour
         DateTime now = DateTime.Now;
         hourNumber = now.Hour;
         minuteNumber = now.Minute;
+
+        //テキストコンポーネント取得
+        tex1 = text1.GetComponent<TextMeshProUGUI>();
+        tex2 = text2.GetComponent<TextMeshProUGUI>();
+
+        tex1.text = "今は" + hourNumber + "時" + minuteNumber + "分です。";
+        tex2.text = "線は" + hourNumber + "本出てきます。";
     }
 
     // Update is called once per frame
