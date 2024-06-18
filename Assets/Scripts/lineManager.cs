9ffb0ad328b5cb965b37bf8929e59e62d4203406
MeiAkiyama <akimei514@gmail.com>
2024-06-18 12:54:37 +0900

    時間から線を表示するプログラム（lineManager）作成

--- Assets/Scripts/lineManager.cs
+++ Assets/Scripts/lineManager.cs
@@ -7,4 +7,54 @@ public class lineManager : MonoBehaviour
     public enum lineColors{
         Red,Blue,Green,Yellow
     };
+
+    //ü‚Ì”z—ñ
+    public GameObject[] lines;
+    private int nowTime;
+    //public int testTime;
+
+    private void Start()
+    {
+        nowTime = TimeSet.getHour();
+        //nowTime = testTime;
+
+        //ŠÔ‚É‚æ‚éğŒ•ªŠò
+        switch (nowTime)
+        {
+            case 0:
+                setZero(); break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                setOneToFour(); break;
+            default:
+                setTimeNumber(nowTime); break;
+        }
+        Debug.Log(nowTime);
+    }
+
+    private void setZero()
+    {
+        for(int i=0; i<lines.Length; i++)
+        {
+            lines[i].SetActive(true);
+        }
+    }
+
+    private void setOneToFour()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            lines[i].SetActive(true);
+        }
+    }
+
+    private void setTimeNumber(int time)
+    {
+        for(int i = 0; i < time; i++)
+        {
+            lines[i].SetActive(true);
+        }
+    }
 }
